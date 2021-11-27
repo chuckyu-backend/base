1c693405cbc26583c2fc3403c426e25870722803
qiankaiyu <986022405@qq.com>
2021-11-27 11:49:19 +0800

    feat(feat)：审计基类

---
+++ src/main/java/com/chuck/base/domain/core/AuditEntity.java
@@ -0,0 +1,24 @@
+package com.chuck.base.domain.core;
+
+import javax.persistence.Embedded;
+import javax.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ *
+ * 如非必要，不推荐继承该类来使用审计功能。而是单独使用审计服务实现
+ *
+ * @author Chuck
+ * @since 6/8/2021
+ * @version 0.0.1
+ *
+ **/
+@MappedSuperclass
+@Getter
+@Setter
+public abstract class AuditEntity extends IdentifiedEntity{
+ @Embedded
+ private Audit audit;
+}
